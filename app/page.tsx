06175e20a418ce70cef48ebf84053d46b960eecc
MrMoradi <MrMohammad.Moradi@yahoo.com>
2025-12-08 21:16:56 +0330

    chore: Next.js project
    - install and configure Next.js 16
    - add TailwindCSS with base config
    - setup ESLint and Prettier
    - create basic folder structure (components, app, lib)

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col bg-white items-center justify-center min-h-screen py-2">
+      <main className="container min-h-[95vh] bg-gray-100"></main>
+    </div>
+  );
+}
