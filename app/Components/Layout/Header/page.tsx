b939be633e572ff2634e66f911ed8a17ea356dc6
MrMoradi <MrMohammad.Moradi@yahoo.com>
2026-01-05 20:09:28 +0330

    feat(ui/Layout/Header): complate header

--- app/Components/Layout/Header/page.tsx
+++ app/Components/Layout/Header/page.tsx
@@ -16,17 +16,15 @@ export const Header = () => {
     <header className="flex flex-col items-center justify-center w-full sticky top-0 z-50 bg-zinc-100 shadow-md">
       <TopBar />
       <main className="container py-2 px-5 hidden lg:flex  justify-between items-center">
-          <Link className="flex flex-col  text-3xl" href={'/'}>
-            <div className="flex">
-              <span className="text-Gold">{Titer[0]}</span>
-              <span className="text-Turquoise">{Titer[1]}</span>
-            </div>
-            <span className="text-lg font-bold text-Lapis mx-auto">
-              {
-                lang === 'fa' ? "GoharSaan" : "گوهرسان"
-              }
-            </span>
-          </Link>
+        <Link className="flex flex-col  text-3xl" href={'/'}>
+          <div className="flex">
+            <span className="text-Gold">{Titer[0]}</span>
+            <span className="text-Turquoise">{Titer[1]}</span>
+          </div>
+          <span className="text-lg font-bold text-Lapis mx-auto">
+            {lang === 'fa' ? 'GoharSaan' : 'گوهرسان'}
+          </span>
+        </Link>
         <NavBar />
         <div className="flex justify-evenly w-40">
           <GroupIcon />
@@ -35,22 +33,20 @@ export const Header = () => {
       </main>
       {/* {header for mobile} */}
       <main className="flex lg:hidden w-full top-0 z-50 px-10 py-2 justify-between items-center bg-zinc-100 shadow-md">
-              <div className="w-32">#</div>
-              <Link className="flex flex-col  text-3xl" href={'/'}>
-            <div className="flex">
-              <span className="text-Gold">{Titer[0]}</span>
-              <span className="text-Turquoise">{Titer[1]}</span>
-            </div>
-            <span className="text-lg font-bold text-Lapis mx-auto">
-              {
-                lang === 'fa' ? "GoharSaan" : "گوهرسان"
-              }
-            </span>
-          </Link>
-          <div className="flex gap-5 items-center w-32">
+        <div className="w-32">#</div>
+        <Link className="flex flex-col  text-3xl" href={'/'}>
+          <div className="flex">
+            <span className="text-Gold">{Titer[0]}</span>
+            <span className="text-Turquoise">{Titer[1]}</span>
+          </div>
+          <span className="text-lg font-bold text-Lapis mx-auto">
+            {lang === 'fa' ? 'GoharSaan' : 'گوهرسان'}
+          </span>
+        </Link>
+        <div className="flex gap-5 items-center w-32">
           <LanguageSwitcher />
           <ShoppingBasketIcon />
-          </div>
+        </div>
       </main>
     </header>
   );
