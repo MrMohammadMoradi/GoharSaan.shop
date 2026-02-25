d437b3b2feed9a4c438ea11e1c82c3c141960611
MrMoradi <MrMohammad.Moradi@yahoo.com>
2026-02-25 22:37:43 +0330

    feat(ui/loading): add loading component for route

---
+++ app/shop/loading.tsx
@@ -0,0 +1,11 @@
+// components/Loading.jsx
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
+      <div className="flex flex-col items-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-4 border-blue-500 border-t-transparent"></div>
+        <p className="mt-4 text-gray-600">در حال بارگذاری...</p>
+      </div>
+    </div>
+  );
+}
